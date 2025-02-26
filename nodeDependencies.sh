21100c82e0397a83c3072ff518eee5ce117a8021
mdieperi <m.m.dieperink@umcutrecht.nl>
2025-02-26 09:14:51 +0100

    test 4

--- nodeDependencies.sh
+++ nodeDependencies.sh
@@ -6,11 +6,11 @@ sudo apt install -y gcc g++ cmake pkg-config curl wget
 sudo snap install go --classic
 sudo apt install -y openssl libssl-dev
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 sudo apt install -y sqlite3 libsqlite3-dev
 sudo apt install -y python3
-curl --proto ‘=https’ –tlsv1.2 -sSf https://sh.rustup.rs | sh
-. ”$HOME/.cargo/env”>
+curl --proto ‘=https’ –tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+#. ”$HOME/.cargo/env”
 sudo apt install -y docker.io docker-buildx
 sudo systemctl enable docker --now
 sudo systemctl start docker
