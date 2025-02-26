697489d60cf43dd82d5e26eff192408789e53b0e
mdieperi <m.m.dieperink@umcutrecht.nl>
2025-02-26 09:06:06 +0100

    test 2: added next dependencies

--- nodeDependencies.sh
+++ nodeDependencies.sh
@@ -3,12 +3,12 @@
 echo "Installing dependencies"
 sudo apt update -y
 sudo apt install -y gcc g++ cmake pkg-config curl wget
-#sudo snap install go --classic
-#sudo apt install -y openssl libssl-dev
-#wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-#sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64
-#sudo apt install -y sqlite3 libsqlite3-dev
-#sudo apt install -y python3
+sudo snap install go --classic
+sudo apt install -y openssl libssl-dev
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64
+sudo apt install -y sqlite3 libsqlite3-dev
+sudo apt install -y python3
 #curl --proto ‘=https’ –tlsv1.2 -sSf https://sh.rustup.rs | sh
 #. ”$HOME/.cargo/env”>
 #sudo apt install -y docker.io docker-buildx
