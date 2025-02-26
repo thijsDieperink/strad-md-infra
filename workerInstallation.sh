70bd969981f4ebfabf5ae48a7573b3941b96d657
mdieperi <m.m.dieperink@umcutrecht.nl>
2025-02-26 11:43:38 +0100

    test 3

--- workerInstallation.sh
+++ workerInstallation.sh
@@ -6,14 +6,16 @@ echo "Lets install the worker node itself"
 echo "install branectl"
 sudo curl -Lo /usr/local/bin/branectl https://github.com/epi-project/brane/releases/latest/download/branectl-linux-x86_64
 sudo chmod +x /usr/local/bin/branectl
-git clone https://github.com/epi-project/brane & cd brane
+git clone https://github.com/epi-project/brane
+sleep 5
+cd brane
 
 # Brane worker stuff
-#echo "create brane worker files"
-#branectl generate backend -f -p ./config/backend.yml local
-#branectl generate proxy -f -p ./config/proxy.yml
+echo "create brane worker files"
+branectl generate backend -f -p ./config/backend.yml local
+branectl generate proxy -f -p ./config/proxy.yml
 #secrets stuff
-#branectl generate policy_db -f -p ./policies.db
+branectl generate policy_db -f -p ./policies.db
 # add ip address
 #branectl generate node -f -H worker.nl:{ipAddress} worker worker.nl workerNode
 
