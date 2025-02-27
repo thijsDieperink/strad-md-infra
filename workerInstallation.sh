9197858289e861993ae163511cf3ec9950bc2753
mdieperi <m.m.dieperink@umcutrecht.nl>
2025-02-27 09:15:18 +0100

    next test

--- workerInstallation.sh
+++ workerInstallation.sh
@@ -6,8 +6,8 @@ echo "Lets install the worker node itself"
 echo "install branectl"
 #sudo curl -Lo /usr/local/bin/branectl https://github.com/epi-project/brane/releases/latest/download/branectl-linux-x86_64
 #sudo chmod +x /usr/local/bin/branectl
-##git clone https://github.com/epi-project/brane
-##cd brane
+git clone https://github.com/epi-project/brane
+cd brane
 #make brane-ctl PROFILE=release
 #sudo mv targets/release/branectl /usr/local/bin/branectl
 #sudo chmod +x /usr/local/bin/branectl
@@ -21,8 +21,11 @@ branectl generate policy_secret -f -p ./config/policy_expert_secret.json
 branectl generate policy_db -f -p ./policies.db
 branectl generate node -f -H worker.nl:$ipAddress worker worker.nl workerNode
 
-#echo "create certificate"
+# Generate certificate
+echo "create certificate"
+mkdir config/certs & cd config/certs
+branectl generate certs client workerNode -H worker.nl -f -p ./client-certs
 
 # Make the images for the worker
 echo "make the necessary images"
-#make central-images PROFILE=release
+make central-images PROFILE=release
